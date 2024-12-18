483af20331abad14b330023c8b4041338cf963a1
Damilola91 <damilola.wiwoloku@gmail.com>
2024-12-18 14:06:41 +0100

    newsletter

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import ProductCard from "../ProductCard/ProductCard";
 import PopularCategories from "../PopularCategories/PopularCategories";
 import SuperDelicious from "../SuperDelicious/SuperDelicious";
-import NewsLetter from "../Pages/NewsLetter/Newsletter";
+import NewsLetter from "../NewsLetter/Newsletter";
 import "./Main.css";
 
 const Main = () => {
